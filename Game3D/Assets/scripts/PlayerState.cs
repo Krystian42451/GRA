1c958268c62ec5ceeb39fca4efee79709dbb1f54
Krystian42451 <kk42451@zut.edu.pl>
2025-01-23 19:42:26 +0100

    Pasek zdrowia

---
+++ Game3D/Assets/scripts/PlayerState.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    public static PlayerState Instance { get; private set; }
+
+    // -------- Player Health -------- //
+    public float currentHealth;
+    public float maxHealth;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        Debug.Log($"Current Health: {currentHealth}/{maxHealth}");
+
+        // Obs³uga wciœniêcia klawisza N
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            currentHealth -= 10;
+            if (currentHealth < 0)
+            {
+                currentHealth = 0; // Zapobieganie ujemnym wartoœciom
+                
+            }
+        }
+
+        if (IsDead())
+        {
+            Debug.Log("Player is dead!");
+            // Tutaj mo¿esz dodaæ np. restart gry
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        currentHealth += amount;
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+
+    public bool IsDead()
+    {
+        return currentHealth <= 0;
+    }
+}
